de6fc2d27a38273c40af6f9764393723c44eda5d
pk33n <mas1jacoke@naver.com>
2020-10-29 21:33:51 +0900

    2차 과제 어렵다

---
+++ 2nd_assignment/2nd_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2nd_assignment
+//
+//  Created by Seri Park on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
